f46c8add94404505d890f785574300eac1c1a7bb
iamfiro <appturbo102@gmail.com>
2024-04-03 23:41:00 +0900

    🚑 Update imports and add type module to package.json

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { IgApiClient } = require('instagram-private-api');
-const { exec } = require('child_process');
-const fs = require('fs');
-var cron = require('node-cron');
-const { config } = require('./config/config');
+import { IgApiClient } from 'instagram-private-api';
+import { exec } from 'child_process';
+import fs from 'fs';
+import cron from 'node-cron';
+import { config } from './config/config.js';
 
 function dayToKorean(day) {
     switch (day) {
@@ -23,8 +23,6 @@ function dayToKorean(day) {
     }
 }
 
-// 코드 이게 끝임
-
 const postToInstagram = async () => {
     const date = new Date();
     console.log('🐍 Python 실행 요청됨')
@@ -38,39 +36,44 @@ const postToInstagram = async () => {
 
         instagram.state.generateDevice(config.instagram.username);
     
-        await instagram.account.login(config.instagram.username, config.instagram.password);
+        await instagram.account.login(config.instagram.username, config.instagram.password).catch((err) => {
+            console.error('🛑 로그인에 실패했습니다');
+            return;
+        }).then(async () => {
+            console.log('✅ 인스타그램 로그인 성공');
+
+            const food = fs.readFileSync('build/meal.jpeg');
+
+            // await instagram.publish.album({
+            //     items: [
+            //         { width: 1024, height: 1024, file: food}
+            //     ],
+            //     caption: 'Test Image', // nice caption (optional)
+            // });
+            
+            console.log('📷 인스타그램에 게시물 올리는 중')
     
-        const food = fs.readFileSync('build/meal.jpeg');
-
-        // await instagram.publish.album({
-        //     items: [
-        //         { width: 1024, height: 1024, file: food}
-        //     ],
-        //     caption: 'Test Image', // nice caption (optional)
-        // });
-        
-        console.log('📷 인스타그램에 게시물 올리는 중')
-
-        const todayDate = `${date.getFullYear()}년 ${String(date.getMonth() + 1).padStart(2, '0')}월 ${String(date.getDate()).padStart(2, '0')}일 ${dayToKorean(date.getDay())}`;
-
-        await instagram.publish.photo({
-            file: food,
-            caption: `${config.schoolName} 오늘의 정보\n\n${todayDate}\n\n#${config.schoolName} #급식표 #밥밥밥`, // nice caption (optional)
-        }).then(() => {
-            if(config.discord.on) {
-                fetch(config.discord.webhook, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        content: `✅ 인스타그램에 게시물이 성공적으로 업로드 되었습니다! (**${todayDate}**)`
-                    })
-                });
-            }
-            console.log('✅ 인스타그램에 게시물 성공적으로 업로드 됨')
-        }).catch((err) => {
-            console.error(err)
+            const todayDate = `${date.getFullYear()}년 ${String(date.getMonth() + 1).padStart(2, '0')}월 ${String(date.getDate()).padStart(2, '0')}일 ${dayToKorean(date.getDay())}`;
+    
+            await instagram.publish.photo({
+                file: food,
+                caption: `${config.schoolName} 오늘의 정보\n\n${todayDate}\n\n#${config.schoolName} #급식표 #밥밥밥`, // nice caption (optional)
+            }).then(() => {
+                if(config.discord.on) {
+                    fetch(config.discord.webhook, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            content: `✅ 인스타그램에 게시물이 성공적으로 업로드 되었습니다! (**${todayDate}**)`
+                        })
+                    });
+                }
+                console.log('✅ 인스타그램에 게시물 성공적으로 업로드 됨')
+            }).catch((err) => {
+                console.error(err)
+            });
         });
     })
 }
