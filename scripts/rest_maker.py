5fa04bf74520d2b340cb4bdd72b0e80faa25140d
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-10-01 15:05:30 +0900

    feat: 휴일이 없는 경우 처리

--- scripts/rest_maker.py
+++ scripts/rest_maker.py
@@ -9,7 +9,7 @@ def loadfont(fontsize):
 
 weekdays = ['월', '화', '수', '목', '금']
 
-def rest(lst, date):
+def rest(lst, date, is_empty):
     W = 1024
     H = 1024
 
@@ -30,13 +30,16 @@ def rest(lst, date):
 
     lst = reversed(lst)
 
-    for l in lst:
-        weekday = weekdays[datetime.strptime(l['date'], '%Y-%m-%d').weekday()]
-        dayNumber = int(l['date'].split('-')[2])
-        content = l['content']
+    if is_empty:
+        draw.text((75, H - 75 - text_l), '휴일이 없어요 ㅠㅠ', font=rest_font, fill=rest_font_color)
+    else:
+        for l in lst:
+            weekday = weekdays[datetime.strptime(l['date'], '%Y-%m-%d').weekday()]
+            dayNumber = int(l['date'].split('-')[2])
+            content = l['content']
 
-        draw.text((75, H - 75 - text_l), f'{dayNumber}일 ({weekday}) - {content}', font=rest_font, fill=rest_font_color)
-        text_l += 85 
+            draw.text((75, H - 75 - text_l), f'{dayNumber}일 ({weekday}) - {content}', font=rest_font, fill=rest_font_color)
+            text_l += 85
 
     image.convert('RGB').save('./build/rest.jpeg', format='JPEG', quality=95)
 
@@ -55,10 +58,12 @@ def transform_meal_data(response_data):
 def get_rest_json():
     today = datetime.today()
     date = today.strftime('%Y-%m-%d')
-    response = requests.get('http://127.0.0.1:8030/meal/rest')
-    transformed_data = transform_meal_data(response.json())
-
-    rest(transformed_data, date)
+    response = requests.get('https://api.sunrin.kr/meal/rest/next')
+    if response['data'] == []:
+        rest([], date, True)
+    else:
+        transformed_data = transform_meal_data(response.json())
+        rest(transformed_data, date, False)
 
 def main():
     if not os.path.exists('./build/'):
